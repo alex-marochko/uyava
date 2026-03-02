6fb8fdb99009bf597c1d3a29bdafcf11f497bb50
Alex Marochko <marochko@gmail.com>
2026-03-02 23:39:27 +0200

    Refine contact channels and remove personal name from public UI

--- site/src/config.ts
+++ site/src/config.ts
@@ -4,8 +4,10 @@ export const SITE = {
     'A developer tool for Flutter that visualizes architecture and event flows with a DevTools extension plus a free desktop app and Pro upgrade.',
   url: 'https://uyava.io',
   github: 'https://github.com/alex-marochko/uyava',
+  githubCoreIssues: 'https://github.com/alex-marochko/uyava/issues',
+  githubDesktopIssues:
+    'https://github.com/alex-marochko/uyava-desktop-issues/issues',
   supportEmail: 'support@uyava.io',
-  operator: 'Oleksandr Marochko',
 };
 
 export const ANALYTICS = {
